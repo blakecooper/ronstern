a035e99617991822dbf22a1a286d15b6e0f48d57
Blake <contact@blake-cooper.com>
2019-08-19 08:29:12 -0400

    scrolling demo

--- scroll.js
+++ scroll.js
@@ -1,33 +1,52 @@
-	const canvas = document.getElementById("layer2");
+const canvas = document.getElementById("layer2");
     
-	const lines = [
-        "While I've",
-        "down loads from the cloud,",
-        "I'm not completely out of the",
-        "woods just yet...",
-        "(site coming soon)"
-    ];
+const lines = [
+    "While I've",
+    "down loads from the cloud,",
+    "I'm not completely out of the",
+    "woods just yet...",
+    "(site coming soon)"
+];
     
-    var font = {
-        "type": "Lexend Tera",
-        "size": 32,
-        "padding": 0,
-        "color": "#ffffff"
-    }
+var font = {
+    "type": "Arial",
+    "size": 32,
+    "padding": 0,
+    "color": "#ffffff"
+}
 
-    var TEXT_X_INITIAL_POS = canvas.width;
-    var TEXT_Y_INITIAL_POS = canvas.height;
+const roadLineColor = "#B89132";
 
-	var context = canvas.getContext("2d");
+var TEXT_X_INITIAL_POS = canvas.width;
+var TEXT_Y_INITIAL_POS = canvas.height;
 
-    context.font = font.size + "px " + font.type;
-    context.fillStyle = font.color;
 
-    for (var x = 0; x < lines.length; x++) {
-        const line = lines[x];
-        const lineOffsetY = lines.length - x;
-        var fontX = TEXT_X_INITIAL_POS - context.measureText(line).width;
-        var fontY = TEXT_Y_INITIAL_POS - ((font.size + font.padding) * lineOffsetY);
-        context.fillText(line, fontX, fontY);
-    };
+var context = canvas.getContext("2d");
 
+context.font = font.size + "px " + font.type;
+context.fillStyle = font.color;
+
+setInterval(drawText,50);
+
+
+
+function drawText() {
+
+	context.clearRect(0,0,canvas.width, canvas.height);
+	
+	for (var x = 0; x < lines.length; x++) {
+    	const line = lines[x];
+	    const lineOffsetY = lines.length - x;
+	    var fontX = TEXT_X_INITIAL_POS - context.measureText(line).width;
+	    var fontY = TEXT_Y_INITIAL_POS - ((font.size + font.padding) * lineOffsetY);
+		context.globalAlpha = fontY / canvas.height;
+		if (x == (lines.length - 1)) {
+			context.fillStyle = roadLineColor;
+		}
+	    context.fillText(line, fontX, fontY);
+
+		context.fillStyle = font.color;
+	};
+
+	TEXT_Y_INITIAL_POS -= 1;
+}
