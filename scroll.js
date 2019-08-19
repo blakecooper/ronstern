42d60a1cd3b699a73f56b4fce4ff82ab8405b9a7
Blake <contact@blake-cooper.com>
2019-08-19 11:01:02 -0400

    refactoring

--- scroll.js
+++ scroll.js
@@ -1,6 +1,4 @@
-const canvas = document.getElementById("layer2");
-    
-const lines = [
+const text = [
     "While I've",
     "down loads from the cloud,",
     "I'm not completely out of the",
@@ -17,36 +15,62 @@ var font = {
 
 const roadLineColor = "#B89132";
 
+const canvas = document.getElementById("layer2"); 
+const context = setInitialContext();
+
 var TEXT_X_INITIAL_POS = canvas.width;
 var TEXT_Y_INITIAL_POS = canvas.height;
 
-
-var context = canvas.getContext("2d");
-
-context.font = font.size + "px " + font.type;
-context.fillStyle = font.color;
-
 setInterval(drawText,50);
 
+function setInitialContext() {
 
+	var ctx = canvas.getContext("2d");
+
+	ctx.font = font.size + "px " + font.type;
+	ctx.fillStyle = font.color;
+
+	return ctx;
+}
 
 function drawText() {
 
-	context.clearRect(0,0,canvas.width, canvas.height);
+	clearCanvas();
 	
-	for (var x = 0; x < lines.length; x++) {
-    	const line = lines[x];
-	    const lineOffsetY = lines.length - x;
-	    var fontX = TEXT_X_INITIAL_POS - context.measureText(line).width;
-	    var fontY = TEXT_Y_INITIAL_POS - ((font.size + font.padding) * lineOffsetY);
-		context.globalAlpha = fontY / canvas.height;
-		if (x == (lines.length - 1)) {
+	for (var x = 0; x < text.length; x++) {
+    	const textLine = text[x];		
+		const textLinePosition = getTextPosition(textLine, x);
+
+		setGlobalAlpha(textLinePosition);
+
+		if (isLastLineOfText(text, x)) {
 			context.fillStyle = roadLineColor;
 		}
-	    context.fillText(line, fontX, fontY);
+
+	    context.fillText(textLine, textLinePosition.X, textLinePosition.Y);
 
 		context.fillStyle = font.color;
 	};
 
 	TEXT_Y_INITIAL_POS -= 1;
+}
+
+function clearCanvas() {
+	context.clearRect(0,0,canvas.width, canvas.height);
+}
+
+function getTextPosition(textLine, textLineNumber) {
+	const textLineOffsetY = text.length - textLineNumber;
+	var textX = TEXT_X_INITIAL_POS - context.measureText(textLine).width;
+	var textY = TEXT_Y_INITIAL_POS - ((font.size + font.padding) * textLineOffsetY);
+
+	return {"X": textX, "Y": textY};
+}
+
+function setGlobalAlpha(textLinePosition) {
+	context.globalAlpha = textLinePosition.Y / canvas.height;
+}
+
+function isLastLineOfText(text, textLineNumber) {
+	return textLineNumber == text.length - 1;
 }
