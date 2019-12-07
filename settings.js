274c519f87deadc6a422e3851162191270e2766e
blakecooper <accounts@blake-cooper.com>
2019-12-07 11:23:58 -0500

    Adjusting Scott transition speed

--- settings.js
+++ settings.js
@@ -138,7 +138,7 @@ let text = {
         "y": 10,
         "transitionInIsComplete": false,
         "transitionOutIsComplete": false,
-		"timingOffset": -30,
+		"timingOffset": 0,
     },
     "8": {
         "line": "",
