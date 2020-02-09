154c42420537abf2cb2fcfd4421bac84e3b256b9
blakecooper <accounts@blake-cooper.com>
2020-02-09 16:04:48 -0500

    Made captions closer to photos to fit on Ron's screen

--- js/animate/captions.js
+++ js/animate/captions.js
@@ -1,7 +1,7 @@
 function positionAndDisplayCaption (photo) {
 	let id = "caption" + photo.captionID;
 	
-	document.getElementById(id).style.top = (photo.drawHeight - 20) + "px";
+	document.getElementById(id).style.top = (photo.drawHeight - 50) + "px";
 
 	if (screenOrientation !== LANDSCAPE) {
 		document.getElementById(id).style.right = (window.innerWidth - photo.drawWidth + 20) + 'px';
