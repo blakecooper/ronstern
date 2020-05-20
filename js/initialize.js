8cfa547816aefb0810337fd7467f8c1541d85f8d
blakecooper <accounts@blake-cooper.com>
2020-05-20 12:01:19 -0400

    Edits to Don photo, changed duration of Bed photo (#10) by +225

--- js/initialize.js
+++ js/initialize.js
@@ -505,7 +505,7 @@ const image = {
             "transitionOutIsComplete": false,
             "transitionSpeed": DEFAULT_PHOTO_TRANSITION_SPEED,
         	"fadeSpeed": .005,
-        	"duration": (DEFAULT_PHOTO_DURATION / 2) - 25,
+        	"duration": (DEFAULT_PHOTO_DURATION / 2) + 200,
 			"hasText": false,
             "centered": true,
 			"textOnRight": true,
